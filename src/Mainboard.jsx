05a2674df30b4ec293ed07e606b34f37af2dc216
Saba Komladze <sabakomladze2@gmail.com>
2023-06-03 16:11:17 +0400

    tablet version

--- src/Mainboard.jsx
+++ src/Mainboard.jsx
@@ -34,7 +34,8 @@ function Mainboard(props) {
         </div>
       </div>
       <section className="mainData">
-        {props.user.bio}
+        <div className="bio">{props.user.bio}</div>
+
         {/* stats*/}
         <div className="stats">
           <div className="repos">
@@ -52,38 +53,82 @@ function Mainboard(props) {
         </div>
         {/* contact*/}
         <div className="contact">
-          <div className="loc">
-            <img
-              src="./assets/icon-location.svg"
-              alt="location mark"
-              className="location"
-            />
-            <p className="city  text">{props.user.location}</p>
-          </div>
-          <div className="loc">
-            <img
-              src="./assets/icon-website.svg"
-              alt="website"
-              className="web"
-            />
-            <p className="weblink  text">{props.user.blog}</p>
-          </div>
-          <div className="loc">
-            <img
-              src="./assets/icon-twitter.svg"
-              alt="twitter logo"
-              className="tw"
-            />
-            <p className="twitter  text">{props.user.twitter_username}</p>
-          </div>
-          <div className="loc">
-            <img
-              src="./assets/icon-company.svg"
-              alt="company logo"
-              className="build"
-            />
-            <p className="company text">{props.user.company}</p>
-          </div>
+          {props.user.location ? (
+            <div className="loc one">
+              <img
+                src="./assets/icon-location.svg"
+                alt="location mark"
+                className="location"
+              />
+              <p className="city  text">{props.user.location}</p>
+            </div>
+          ) : (
+            <div className="error">
+              <img
+                src="./assets/icon-location.svg"
+                alt="location mark"
+                className="location errimg"
+              />
+              <p className="errormsg first">Not Available</p>
+            </div>
+          )}
+          {props.user.blog ? (
+            <div className="loc second">
+              <img
+                src="./assets/icon-website.svg"
+                alt="website"
+                className="web"
+              />
+              <p className="weblink  text">{props.user.blog}</p>
+            </div>
+          ) : (
+            <div className="error">
+              <img
+                src="./assets/icon-website.svg"
+                alt="website"
+                className="web  errimg"
+              />
+              <p className="errormsg">Not Available</p>
+            </div>
+          )}
+          {props.user.twitter_username ? (
+            <div className="loc third">
+              <img
+                src="./assets/icon-twitter.svg"
+                alt="twitter logo"
+                className="tw"
+              />
+              <p className="twitter  text">{props.user.twitter_username}</p>
+            </div>
+          ) : (
+            <div className="error">
+              <img
+                src="./assets/icon-twitter.svg"
+                alt="twitter logo"
+                className="tw  errimg"
+              />
+              <p className="errormsg">Not Available</p>
+            </div>
+          )}
+          {props.user.company ? (
+            <div className="loc fourth">
+              <img
+                src="./assets/icon-company.svg"
+                alt="company logo"
+                className="build"
+              />
+              <p className="company text">{props.user.company}</p>
+            </div>
+          ) : (
+            <div className="error">
+              <img
+                src="./assets/icon-company.svg"
+                alt="company logo"
+                className="build  errimg"
+              />
+              <p className="errormsg">Not Available</p>
+            </div>
+          )}
         </div>
       </section>
     </div>
